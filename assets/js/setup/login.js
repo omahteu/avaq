032198403f61962892f96b28ca07d1765e8fb62d
matheus <matheus.mmn@hotmail.com>
2022-05-30 15:21:42 -0300

    implemetacao de login e layout alunos

---
+++ assets/js/setup/login.js
@@ -0,0 +1,12 @@
+$("#acessar").click(function(){
+    let usuario = $("#usuario").val()
+    let senha = $(("#senha")).val()
+    
+    if(usuario == "prof" && senha == "123"){
+        $(location).attr('href', "./professor.html")
+    } else if(usuario == "aluno" && senha == "123"){
+        $(location).attr("href", "./aluno.html")
+    } else {
+        alert("Login Inválido!")
+    }
+})
