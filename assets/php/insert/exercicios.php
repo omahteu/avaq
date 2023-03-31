d18268e8d804d5b6a4e9ea388e83b02dfc47bf4c
matheus <matheus.mmn@hotmail.com>
2023-03-31 16:50:19 -0300

    ajustes

--- assets/php/insert/exercicios.php
+++ assets/php/insert/exercicios.php
@@ -2,6 +2,7 @@
 include "../conexao.php";
 
 $ds = $_POST["qd"];
+$cp = $_POST["cap"];
 $q1 = $_POST["q1"];
 $q2 = $_POST["q2"];
 $q3 = $_POST["q3"];
@@ -12,14 +13,14 @@ $q7 = $_POST["q7"];
 $q8 = $_POST["q8"];
 $q9 = $_POST["q9"];
 $q10 = $_POST["q10"];
-$query = "insert into avaqce_exercicios(disciplina, um, dois, tres, quatro, cinco, seis, sete, oito, nove, dez) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$query = "insert into avaqce_exercicios(disciplina, capitulo, um, dois, tres, quatro, cinco, seis, sete, oito, nove, dez) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 if ($conn->connect_error) {
     echo "$conn->connect_error";
     die("Connection Failed : " . $conn->connect_error);
 } else {
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("sssssssssss", $ds, $q1, $q2, $q3, $q4, $q5, $q6, $q7, $q8, $q9, $q10);
+    $stmt->bind_param("ssssssssssss", $ds, $cp, $q1, $q2, $q3, $q4, $q5, $q6, $q7, $q8, $q9, $q10);
     $execval = $stmt->execute();
     $stmt->close();
     $conn->close();
