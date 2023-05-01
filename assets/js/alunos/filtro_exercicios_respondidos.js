cd676307a19216d9f3fe656c12030e65ac8f5f0e
Matheus Martins <matheus.mmn@hotmail.com>
2023-05-01 09:01:28 -0600

    página de chat e de provas

--- assets/js/alunos/filtro_exercicios_respondidos.js
+++ assets/js/alunos/filtro_exercicios_respondidos.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-    //filtrando_exibicao()
-    // pegarDiferenca()
-})
-
 export default async function filtrando_exibicao(){
     var caixa1 = []
     var caixa2 = []
@@ -19,19 +14,7 @@ export default async function filtrando_exibicao(){
         res2.forEach(e => {
             caixa2.push(e.cod)
         });
-
-
         let r3 = caixa1.filter( a => !caixa2.includes( a ) );
         return r3
     }
-}
-
-function pegarDiferenca() {
-    let r1 = [2,4,6,8];
-    let r2 = [3,4,5,7,9];       
-    let r3 = r1.filter( a => !r2.includes( a ) );
-    
-    console.log(r1, r2)
-    console.log('*********************')
-    console.log(r3)
 }
