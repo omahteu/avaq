ed86e3ea26130d909256071bfafd44d3a3104002
Matheus Martins <matheus.mmn@hotmail.com>
2023-05-15 21:25:26 -0600

    ajustes na exibição e envio das respostas

--- assets/js/alunos/carrega_exercicios.js
+++ assets/js/alunos/carrega_exercicios.js
@@ -1,26 +1,136 @@
-import {disc} from "../disciplinas.js"
-import filtrando_exibicao from "./filtro_exercicios_respondidos.js"
+import { disc } from "../disciplinas.js";
+import filtrando_exibicao from "./filtro_exercicios_respondidos.js";
 
 $(document).ready(function () {
-    carregar()
-})
+    carregar();
+});
 
 async function carregar() {
-    const rq = await fetch("http://localhost/avaq/assets/php/read/exercicios.php")
-    const rs = await rq.json()
-    if (rs["status"] == true) {
-        let exe = rs["dados"]
-        var nome = sessionStorage.getItem("logado")
+    var caixa1 = [];
+    var caixa2 = [];
+    const rq1 = await fetch(
+        "http://localhost/avaq/assets/php/read/exercicios.php"
+    );
+    const rq2 = await fetch(
+        "http://localhost/avaq/assets/php/read/respostas.php"
+    );
+    const rs1 = await rq1.json();
+    const rs2 = await rq2.json();
+    let nome = sessionStorage.getItem("logado");
+    if (rs1["status"] && rs2["status"]) {
+        let res1 = rs1["dados"];
+        let res2 = rs2["dados"];
+        res1.forEach((e) => {
+            caixa1.push(e.cod);
+        });
+        res2.forEach((e) => {
+            caixa2.push(e.cod);
+        });
+        let r3 = caixa1.filter((a) => !caixa2.includes(a));
+        res1.forEach((e) => {
+            if (r3.includes(e.cod)) {
+                try {
+                    $("#ficha").append(
+                        `<button type="button" 
+                        class="btn btn-primary confg" 
+                        data-toggle="modal" 
+                        data-target="#tarja${e.capitulo}${e.disciplina}">${
+                            disc[e.disciplina]
+                        } ${e.capitulo}</button>`
+                    );
+                    $("#features-sec").append(
+                        `
+                        <div class="modal fade" id="tarja${e.capitulo}${e.disciplina}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" style="display: none;" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Auto Atividade</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form method="post" id="exerciciosalunos${e.capitulo}${e.disciplina}">
+                                        <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.um}:</label>
+                                            <textarea class="form-control" name="q1" id="q1"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.dois}:</label>
+                                            <textarea class="form-control" name="q2" id="q2"></textarea>
+                                        </div>
+                                            <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.tres}:</label>
+                                            <textarea class="form-control" name="q3" id="q3"></textarea>
+                                        </div>
+                                            <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.quatro}:</label>
+                                            <textarea class="form-control" name="q4" id="q4"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.cinco}:</label>
+                                            <textarea class="form-control" name="q5" id="q5"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.seis}:</label>
+                                            <textarea class="form-control" name="q6" id="q6"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.sete}:</label>
+                                            <textarea class="form-control" name="q7" id="q7"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.oito}:</label>
+                                            <textarea class="form-control" name="q8" id="q8"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.nove}:</label>
+                                            <textarea class="form-control" name="q9" id="q9"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message-text" class="col-form-label">${e.dez}:</label>
+                                            <textarea class="form-control" name="q10" id="q10"></textarea>
+                                        </div>
+                                        <div class="form-group" hidden>
+                                            <input type="text" name="disciplina" id="disciplina" value="${e.disciplina}">
+                                        </div>
+                                        <div class="form-group" hidden>
+                                            <input type="text" name="aluno" id="aluno" value="${nome}">
+                                        </div>
+                                        <div class="form-group" hidden>
+                                            <input type="text" name="hash" id="hash" value="${e.cod}">
+                                        </div>
+                                        <div class="form-group" hidden>
+                                        <input type="text" name="capi" id="capi" value="${e.capitulo}">
+                                    </div>
+                                        <div class="modal-footer">
+                                            <button type="reset" class="btn btn-secondary">Limpar</button>
+                                            <button type="button" class="btn btn-primary" id="respondendo" data-toggle="${e.capitulo}${e.disciplina}">Enviar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        `
+                    );
+                } catch (error) {
+                    sessionStorage.setItem("erro", error);
+                }
+            }
+        });
+    } else {
+        let exe = rs1["dados"];
         try {
-            let bvv = await filtrando_exibicao()
-            let pom = exe.filter(x => x.cod == bvv)
-            pom.forEach(e => {
+            exe.forEach((e) => {
                 $("#ficha").append(
                     `<button type="button" 
                     class="btn btn-primary confg" 
                     data-toggle="modal" 
-                    data-target="#tarja${e.capitulo}${e.disciplina}">${disc[e.disciplina]} ${e.capitulo}</button>`
-                )
+                    data-target="#tarja${e.capitulo}${e.disciplina}">${
+                        disc[e.disciplina]
+                    } ${e.capitulo}</button>`
+                );
                 $("#features-sec").append(
                     `
                     <div class="modal fade" id="tarja${e.capitulo}${e.disciplina}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" style="display: none;" aria-hidden="true">
@@ -83,6 +193,9 @@ async function carregar() {
                                     <div class="form-group" hidden>
                                         <input type="text" name="hash" id="hash" value="${e.cod}">
                                     </div>
+                                    <div class="form-group" hidden>
+                                    <input type="text" name="capi" id="capi" value="${e.capitulo}">
+                                </div>
                                     <div class="modal-footer">
                                         <button type="reset" class="btn btn-secondary">Limpar</button>
                                         <button type="button" class="btn btn-primary" id="respondendo" data-toggle="${e.capitulo}${e.disciplina}">Enviar</button>
@@ -93,10 +206,10 @@ async function carregar() {
                     </div>
                 </div>
                     `
-                )
+                );
             });
         } catch (error) {
-            sessionStorage.setItem("erro", error)
+            sessionStorage.setItem("erro", error);
         }
     }
 }
--- assets/php/insert/respostaexercicios.php
+++ assets/php/insert/respostaexercicios.php
@@ -4,6 +4,7 @@ include "../conexao.php";
 $cd = $_POST["hash"];
 $ds = $_POST["disciplina"];
 $rm = $_POST["aluno"];
+$cp = $_POST["capi"];
 $q1 = $_POST["q1"];
 $q2 = $_POST["q2"];
 $q3 = $_POST["q3"];
@@ -14,14 +15,14 @@ $q7 = $_POST["q7"];
 $q8 = $_POST["q8"];
 $q9 = $_POST["q9"];
 $q10 = $_POST["q10"];
-$query = "insert into avaqce_respostas(cod, rm, disciplina, um, dois, tres, quatro, cinco, seis, sete, oito, nove, dez) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$query = "insert into avaqce_respostas(cod, rm, disciplina, capitulo, um, dois, tres, quatro, cinco, seis, sete, oito, nove, dez) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 if ($conn->connect_error) {
     echo "$conn->connect_error";
     die("Connection Failed : " . $conn->connect_error);
 } else {
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("sssssssssssss", $cd, $rm, $ds, $q1, $q2, $q3, $q4, $q5, $q6, $q7, $q8, $q9, $q10);
+    $stmt->bind_param("ssssssssssssss", $cd, $rm, $ds, $cp,$q1, $q2, $q3, $q4, $q5, $q6, $q7, $q8, $q9, $q10);
     $execval = $stmt->execute();
     $stmt->close();
     $conn->close();
