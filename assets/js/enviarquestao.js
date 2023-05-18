8ff54149c900b29a27903debf2a8c2da455f1334
Matheus Martins <matheus.mmn@hotmail.com>
2023-05-18 16:27:34 -0600

    já está enviando mensagens pelo lado do aluno

--- assets/js/enviarquestao.js
+++ assets/js/enviarquestao.js
@@ -1,13 +1,12 @@
 $(document).on("click", "#comentar", function() {
     let respostas = $("#comentarios").serialize()
-    console.log(respostas)
-    // $.ajax({
-    //     type: 'POST',
-    //     dataType: 'json',
-    //     url: "http://localhost/avaq/assets/php/insert/chat.php",
-    //     async: true,
-    //     data: respostas
-    // });
-    // $("#comentarios"). each (function(){ this. reset(); })
-    // alert("Exercício respondido com sucesso!")
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: "http://localhost/avaq/assets/php/insert/chat.php",
+        async: true,
+        data: respostas
+    });
+    $("#comentarios"). each (function(){ this. reset(); })
+    alert("Exercício respondido com sucesso!")
 })
--- assets/php/insert/chat.php
+++ assets/php/insert/chat.php
@@ -2,16 +2,19 @@
 include "../conexao.php";
 
 $hoje = date('d/m/Y');
+$rm = $_POST["rm"];
+$hs = $_POST["hash"];
 $ds = $_POST["disciplinas"];
 $ms = $_POST["mensagem"];
-$query = "insert into avaqce_chats(data, disciplina, mensagem) values(?, ?, ?)";
+$rs = "";
+$query = "insert into avaqce_chats(rm, hash, data, disciplina, mensagem, resposta) values(?, ?, ?, ?, ?, ?)";
 
 if ($conn->connect_error) {
     echo "$conn->connect_error";
     die("Connection Failed : " . $conn->connect_error);
 } else {
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("sss", $hoje, $ds, $ms);
+    $stmt->bind_param("ssssss", $rm, $hs, $hoje, $ds, $ms, $rs);
     $execval = $stmt->execute();
     $stmt->close();
     $conn->close();
