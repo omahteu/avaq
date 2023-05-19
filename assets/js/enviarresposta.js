ea5f11e1b8220212cd48de4adb1634b46795b3f7
matheus <matheus.mmn@hotmail.com>
2023-05-19 11:56:57 -0300

    urls de alteracoes

--- assets/js/enviarresposta.js
+++ assets/js/enviarresposta.js
@@ -1,10 +1,12 @@
+import {urlAlteracao} from "./urls.js"
+
 $(document).on("click", "#responderchat", function() {
     let respostas = $("#respondendochat").serialize()
     console.log(respostas)
     $.ajax({
         type: 'POST',
         dataType: 'json',
-        url: "http://localhost/avaq/assets/php/update/responderchat.php",
+        url: `${urlAlteracao}responderchat.php`,
         async: true,
         data: respostas
     });
--- assets/js/urls.js
+++ assets/js/urls.js
@@ -1,2 +1,4 @@
 export var urlHomologacaoLeitura = "http://localhost/avaq/assets/php/read/"
 export var urlInsercao = "http://localhost/avaq/assets/php/insert/"
+export var urlAlteracao = "http://localhost/avaq/assets/php/insert/"
+
