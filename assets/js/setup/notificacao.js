ca4c84b2e4368ecad5c26b53b0b6025f2db0df38
Matheus Martins <matheus.mmn@hotmail.com>
2023-05-21 01:04:17 -0600

    ajustes notificacoes

--- assets/js/setup/notificacao.js
+++ assets/js/setup/notificacao.js
@@ -12,11 +12,32 @@ async function notificacoes() {
         let dados = rs["dados"][0]
         let hoje = String(dataHoje())
 
-        if (moment(hoje).isSame(String(dados["aulas_mod1"]))) {
-            console.log('ativar')
-        } else if (moment(hoje).isAfter(String(dados["aulas_mod1"])) && moment(hoje).isBefore(String(dados["aulas_mod2"]))) {
+        if (moment(hoje).isSame(String(dados["modulo1"]))) {
+            console.log('ativar1')
+        } else if (moment(hoje).isAfter(String(dados["modulo1"])) && moment(hoje).isBefore(String(dados["modulo2"]))) {
+            console.log('ativar1')
+        } else if (moment(hoje).isSame(String(dados["modulo2"]))) {
             console.log('ativar2')
+        } else if (moment(hoje).isAfter(String(dados["modulo2"])) && moment(hoje).isBefore(String(dados["modulo3"]))) {
+            console.log('ativar2')
+        } else if (moment(hoje).isSame(String(dados["modulo3"]))) {
+            console.log('ativar3')
+        } else if (moment(hoje).isAfter(String(dados["modulo3"])) && moment(hoje).isBefore(String(dados["modulo4"]))) {
+            console.log('ativar3')
+        } else if (moment(hoje).isSame(String(dados["modulo4"]))) {
+            console.log('ativar4')
+        } else if (moment(hoje).isAfter(String(dados["modulo4"])) && moment(hoje).isBefore(String(dados["modulo5"]))) {
+            console.log('ativar4')
+        } else if (moment(hoje).isSame(String(dados["modulo5"]))) {
+            console.log('ativar5')
+        } else if (moment(hoje).isAfter(String(dados["modulo5"])) && moment(hoje).isBefore(String(dados["modulo6"]))) {
+            console.log('ativar5')
+        } else if (moment(hoje).isSame(String(dados["modulo6"]))) {
+            console.log('ativar6')
+        } else if (moment(hoje).isAfter(String(dados["modulo6"])) && moment(hoje).isBefore(String(dados["final"]))) {
+            console.log('ativar6')
+        } else if (moment(hoje).isSame(String(dados["final"]))) {
+            console.log('bloquear')
         }
-
     }
 }
--- assets/php/read/datas.php
+++ assets/php/read/datas.php
@@ -7,24 +7,13 @@ if ($resultado->num_rows > 0) {
     while($row = $resultado->fetch_assoc()) {
         $dados[] = [
             'id' => $row["id"],
-            'aulas_mod1' => $row["aulas_mod1"],
-            'aulas_mod2' => $row["aulas_mod2"],
-            'aulas_mod3' => $row["aulas_mod3"],
-            'aulas_mod4' => $row["aulas_mod4"],
-            'aulas_mod5' => $row["aulas_mod5"],
-            'aulas_mod6' => $row["aulas_mod6"],
-            'exercicios_mod1' => $row["exercicios_mod1"],
-            'exercicios_mod2' => $row["exercicios_mod2"],
-            'exercicios_mod3' => $row["exercicios_mod3"],
-            'exercicios_mod4' => $row["exercicios_mod4"],
-            'exercicios_mod5' => $row["exercicios_mod5"],
-            'exercicios_mod6' => $row["exercicios_mod6"],
-            'provas_mod1' => $row["provas_mod1"],
-            'provas_mod2' => $row["provas_mod2"],
-            'provas_mod3' => $row["provas_mod3"],
-            'provas_mod4' => $row["provas_mod4"],
-            'provas_mod5' => $row["provas_mod5"],
-            'provas_mod6' => $row["provas_mod6"]
+            'modulo1' => $row["modulo1"],
+            'modulo2' => $row["modulo2"],
+            'modulo3' => $row["modulo3"],
+            'modulo4' => $row["modulo4"],
+            'modulo5' => $row["modulo5"],
+            'modulo6' => $row["modulo6"],
+            'final' => $row["final"]
         ];
     }
     $retorna = ['status' => true, 'dados' => $dados];
